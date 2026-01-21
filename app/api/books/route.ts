26997a5dc7f8b200dbc44b3a254d8882c9f8df67
AHMAD BARIZI <161280645+ahmadbarizi165@users.noreply.github.com>
2026-01-21 20:41:15 +0700

    Update route.ts

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,16 +1,14 @@
- import { connectDB } from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import Book from "@/models/Book";
 
 export async function GET() {
   try {
     await connectDB();
-
     const books = await Book.find().sort({ createdAt: -1 });
-
     return Response.json(books);
   } catch (error) {
     return Response.json(
-      { message: "Gagal mengambil data ebook" },
+      { error: "Gagal mengambil data buku" },
       { status: 500 }
     );
   }

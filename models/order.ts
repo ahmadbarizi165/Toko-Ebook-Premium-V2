aecb6301bfceb3a40e2766e9957e90da4579969a
AHMAD BARIZI <161280645+ahmadbarizi165@users.noreply.github.com>
2026-01-22 08:20:24 +0700

    Update order.ts

--- models/order.ts
+++ models/order.ts
@@ -1,14 +1,5 @@
 import mongoose from "mongoose";
 
-const OrderSchema = new mongoose.Schema({
-  bookId: { type: mongoose.Schema.Types.ObjectId, ref: "Book" },
-  bookTitle: String,
-  price: Number,
-  buyerName: String,
-  buyerEmail: String,
-  status: { type: String, default: "PENDING" }, // PENDING | PAID
-  createdAt: { type: Date, default: Date.now }
-});
+const OrderSchema = new mongoose.Schema({ bookId: { type: mongoose.Schema.Types.ObjectId, ref: "Book" }, bookTitle: String, price: Number, buyerName: String, buyerEmail: String, status: { type: String, default: "PENDING" }, // PENDING | PAID downloadToken: { type: String }, createdAt: { type: Date, default: Date.now }, });
 
-export default mongoose.models.Order ||
-  mongoose.model("Order", OrderSchema);
+export default mongoose.models.Order || mongoose.model("Order", OrderSchema);

83e4b1cbbb5689706880d8a61ff1c6c15e8b649e
AHMAD BARIZI <161280645+ahmadbarizi165@users.noreply.github.com>
2026-01-22 22:49:30 +0700

    Update route.ts

--- app/api/admin/orders/route.ts
+++ app/api/admin/orders/route.ts
@@ -1,9 +1,15 @@
- import { connectDB } from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import Order from "@/models/Order";
 import crypto from "crypto";
 
 export async function POST(req: Request) {
-  const { id } = await req.json();
+  const { id, secret } = await req.json();
+
+  // 🔐 CEK ADMIN SECRET
+  if (secret !== process.env.ADMIN_SECRET) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   await connectDB();
 
   const token = crypto.randomBytes(32).toString("hex");
@@ -11,8 +17,11 @@ export async function POST(req: Request) {
   await Order.findByIdAndUpdate(id, {
     status: "PAID",
     downloadToken: token,
-    tokenExpiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    tokenExpiredAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
   });
 
-  return Response.json({ success: true });
+  return Response.json({
+    success: true,
+    message: "Order berhasil dikonfirmasi PAID",
+  });
 }

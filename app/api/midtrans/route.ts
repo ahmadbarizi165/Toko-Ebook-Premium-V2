7c2b9f7d8f3cc11b35aa2077832eccca9850adec
AHMAD BARIZI <161280645+ahmadbarizi165@users.noreply.github.com>
2026-01-23 21:49:10 +0700

    Update route.ts

--- app/api/midtrans/route.ts
+++ app/api/midtrans/route.ts
@@ -1,38 +1,38 @@
 import { NextResponse } from "next/server";
 import Midtrans from "midtrans-client";
+import { connectDB } from "@/lib/mongodb";
+import Order from "@/models/Order";
 
 export async function POST(req: Request) {
   const body = await req.json();
+  await connectDB();
+
+  const orderId = "ORDER-" + Date.now();
+
+  await Order.create({
+    midtransOrderId: orderId,
+    bookTitle: body.title,
+    price: body.price,
+    buyerEmail: body.email,
+    buyerName: body.name,
+    status: "PENDING",
+  });
 
   const snap = new Midtrans.Snap({
     isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
     serverKey: process.env.MIDTRANS_SERVER_KEY!,
   });
 
-  const orderId = "ORDER-" + Date.now();
-
-  const parameter = {
+  const transaction = await snap.createTransaction({
     transaction_details: {
       order_id: orderId,
       gross_amount: body.price,
     },
     customer_details: {
-      first_name: body.name || "Pembeli Ebook",
-      email: body.email || "email@ebookpremium.id",
+      first_name: body.name,
+      email: body.email,
     },
-    item_details: [
-      {
-        id: body.id,
-        price: body.price,
-        quantity: 1,
-        name: body.title,
-      },
-    ],
-  };
-
-  const transaction = await snap.createTransaction(parameter);
-
-  return NextResponse.json({
-    token: transaction.token,
   });
+
+  return NextResponse.json({ token: transaction.token });
 }
